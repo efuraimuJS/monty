5130c4508850d5bdc6f88eb61ef32a7ad6767399
efuraimujs <efuraimu.js@gmail.com>
2022-06-16 00:21:11 +0300

    Basic & Advance Tasks

--- main.c
+++ main.c
@@ -1,88 +1,74 @@
 #include "monty.h"
+#include "lists.h"
+
+data_t data = DATA_INIT;
 
 /**
-* main - exec Monty code
-* @argc: number of command arguments
-* @argv: vector of command arguments
-* Return: EXIT_SUCCESS on success, EXIT_FAILURE on fail
-*/
-int main(int argc, char *argv[])
+ * monty - helper function for main function
+ * @args: pointer to struct of arguments from main
+ *
+ * Description: opens and reads from the file
+ * containing the opcodes, and calls the function
+ * that will find the corresponding executing function
+ */
+void monty(args_t *args)
 {
-	if (argc != 2)
+	size_t len = 0;
+	int get = 0;
+	void (*code_func)(stack_t **, unsigned int);
+
+	if (args->ac != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
+		dprintf(STDERR_FILENO, USAGE);
 		exit(EXIT_FAILURE);
 	}
-	create_buff(argv[1]);
-	fclose(argument_container.file);
-	return (EXIT_SUCCESS);
-}
-
-/**
-* free_stack - frees memory in the stack
-* @head: the first node
-*/
-void free_stack(stack_t **head)
-{
-	stack_t *temp;
-
-	if (!head)
-		return;
-	free(argument_container.input);
-	while (*head != NULL)
+	data.fptr = fopen(args->av, "r");
+	if (!data.fptr)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		dprintf(STDERR_FILENO, FILE_ERROR, args->av);
+		exit(EXIT_FAILURE);
 	}
-}
-
-/**
-* get_opcode - gets the correct function
-* @stack: the stack
-* @line_number: the line_number
-* @command: the command being searched for
-*/
-void get_opcode(stack_t **stack, unsigned int line_number, char *command)
-{
-	int index = 0;
-	instruction_t codes[] = {
-		{"push", op_push},
-		{"pall", op_pall},
-		{"pint", op_pint},
-		{"pop", op_pop},
-		{"swap", op_swap},
-		{"add", op_add},
-		{"sub", op_sub},
-		{"div", op_sub},
-		{"mul", op_mul},
-		{"nop", op_nop},
-		{"mod", op_mod},
-		{"\0", NULL}
-	};
-
-	if (command[0] == '#')
-		return;
-	if (strcmp(command, "queue") == 0)
+	while (1)
 	{
-		argument_container.stack_queue = 0;
-		return;
-	}
-	if (strcmp(command, "stack") == 0)
-	{
-		argument_container.stack_queue = 1;
-		return;
-	}
-	while (codes[index].opcode != NULL)
-	{
-		if (strcmp(codes[index].opcode, command) == 0)
+		args->line_number++;
+		get = getline(&(data.line), &len, data.fptr);
+		if (get < 0)
+			break;
+		data.words = strtow(data.line);
+		if (data.words[0] == NULL || data.words[0][0] == '#')
 		{
-			codes[index].f(stack, line_number);
-			return;
+			free_all(0);
+			continue;
 		}
-		index++;
+		code_func = get_func(data.words);
+		if (!code_func)
+		{
+			dprintf(STDERR_FILENO, UNKNOWN, args->line_number, data.words[0]);
+			free_all(1);
+			exit(EXIT_FAILURE);
+		}
+		code_func(&(data.stack), args->line_number);
+		free_all(0);
 	}
-	printf("L%d: unknown instruction %s\n", line_number, command);
-	free_stack(stack);
-	exit(EXIT_FAILURE);
+	free_all(1);
+}
+
+/**
+ * main - entry point for monty bytecode interpreter
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char *argv[])
+{
+	args_t args;
+
+	args.av = argv[1];
+	args.ac = argc;
+	args.line_number = 0;
+
+	monty(&args);
+
+	return (EXIT_SUCCESS);
 }
--- monty.h
+++ monty.h
@@ -1,14 +1,11 @@
-#ifndef _MONTY_H_
-#define _MONTY_H_
+#ifndef MONTY_H
+#define MONTY_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <ctype.h>
+#include <unistd.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -41,39 +38,98 @@ typedef struct instruction_s
 } instruction_t;
 
 /**
- * struct GlobalStruct - contains all globals
- * @arg: arguments
- * @input: the input
- * @stack_queue: the stack or queue
- * @file: the file
+ * struct args_s - structure of arguments from main
+ * @av: name of the file from the command line
+ * @ac: number of arguments from main
+ * @line_number: number of the current line in the file
+ *
+ * Description: arguments passed to main from the command line
+ * used in different functions, organized in a struct for clarity
  */
-typedef struct Global_struct
+typedef struct args_s
 {
-	char *arguments;
-	char *input;
-	int stack_queue;
-	FILE *file;
-} global_variables;
+	char *av;
+	int ac;
+	unsigned int line_number;
+} args_t;
 
-extern global_variables argument_container;
-global_variables argument_container;
+/**
+ * struct data_s - extern data to access inside functions
+ * @line: line from the file
+ * @words: parsed line
+ * @stack: pointer to the stack
+ * @fptr: file pointer
+ * @qflag: flag for queue or stack
+ */
+typedef struct data_s
+{
+	char *line;
+	char **words;
+	stack_t *stack;
+	FILE *fptr;
+	int qflag;
+} data_t;
 
-int isnum(char *str);
-void free_stack(stack_t **head);
-void get_opcode(stack_t **stack, unsigned int line_number, char *command);
-void create_buff(char *f_name);
-void op_push(stack_t **stack, unsigned int line_number);
-void op_pall(stack_t **stack, unsigned int line_number);
-void op_pint(stack_t **stack, unsigned int line_number);
-void op_pop(stack_t **stack, unsigned int line_number);
-void op_swap(stack_t **stack, unsigned int line_number);
-void op_nop(stack_t **stack, unsigned int line_number);
-void op_add(stack_t **stack, unsigned int line_number);
-void op_sub(stack_t **stack, unsigned int line_number);
-void op_div(stack_t **stack, unsigned int line_number);
-void op_mul(stack_t **stack, unsigned int line_number);
-void op_mod(stack_t **stack, unsigned int line_number);
-void nasus(stack_t **stack, stack_t *fresh);
-void veigar(stack_t **stack, stack_t *fresh);
+typedef stack_t dlistint_t;
 
-#endif /* Life of Brian */
+extern data_t data;
+
+#define DATA_INIT {NULL, NULL, NULL, NULL, 0}
+
+#define USAGE "USAGE: monty file\n"
+#define FILE_ERROR "Error: Can't open file %s\n"
+#define UNKNOWN "L%u: unknown instruction %s\n"
+#define MALLOC_FAIL "Error: malloc failed\n"
+#define PUSH_FAIL "L%u: usage: push integer\n"
+#define PINT_FAIL "L%u: can't pint, stack empty\n"
+#define POP_FAIL "L%u: can't pop an empty stack\n"
+#define SWAP_FAIL "L%u: can't swap, stack too short\n"
+#define ADD_FAIL "L%u: can't add, stack too short\n"
+#define SUB_FAIL "L%u: can't sub, stack too short\n"
+#define DIV_FAIL "L%u: can't div, stack too short\n"
+#define DIV_ZERO "L%u: division by zero\n"
+#define MUL_FAIL "L%u: can't mul, stack too short\n"
+#define MOD_FAIL "L%u: can't mod, stack too short\n"
+#define PCHAR_FAIL "L%u: can't pchar, stack empty\n"
+#define PCHAR_RANGE "L%u: can't pchar, value out of range\n"
+
+/* main.c */
+void monty(args_t *args);
+
+/* get_func.c */
+void (*get_func(char **parsed))(stack_t **, unsigned int);
+void push_handler(stack_t **stack, unsigned int line_number);
+void pall_handler(stack_t **stack, unsigned int line_number);
+
+/* handler_funcs1.c */
+void pint_handler(stack_t **stack, unsigned int line_number);
+void pop_handler(stack_t **stack, unsigned int line_number);
+void swap_handler(stack_t **stack, unsigned int line_number);
+void add_handler(stack_t **stack, unsigned int line_number);
+void nop_handler(stack_t **stack, unsigned int line_number);
+
+/* handler_funcs2.c */
+void sub_handler(stack_t **stack, unsigned int line_number);
+void div_handler(stack_t **stack, unsigned int line_number);
+void mul_handler(stack_t **stack, unsigned int line_number);
+void mod_handler(stack_t **stack, unsigned int line_number);
+
+/* handler_funcs3.c */
+void rotl_handler(stack_t **stack, unsigned int line_number);
+void rotr_handler(stack_t **stack, unsigned int line_number);
+void stack_handler(stack_t **stack, unsigned int line_number);
+void queue_handler(stack_t **stack, unsigned int line_number);
+
+/* char.c */
+void pchar_handler(stack_t **stack, unsigned int line_number);
+void pstr_handler(stack_t **stack, unsigned int line_number);
+
+/* strtow.c */
+int count_word(char *s);
+char **strtow(char *str);
+void free_everything(char **args);
+
+/* free.c */
+void free_all(int all);
+
+#endif
